dae5601ffabe8dea52bf6cf7b96a9f6d3329e372
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-09-24 08:46:27 +0100

    Task 100

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,7 @@
 export default function createIteratorObject(report) {
   let employees = [];
   for (const item of Object.values(report.employees)) {
-    employees = [
-      ...employees,
-      ...item,
-    ];
+    employees = [...employees, ...item];
   }
   return employees;
 }
