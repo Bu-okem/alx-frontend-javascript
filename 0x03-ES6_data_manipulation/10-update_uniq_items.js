31c1afa76e7da94ec9d1605bdbfc89181a8a016b
bu-okem <chiobiegbulem@gmail.com>
2024-05-07 00:03:59 +0100

    feat: task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,12 @@
-const updateUniqueItems = (map) => {
-  if (!(map instanceof Map)) throw new Error('Cannot process');
-
-  map.forEach((value, key) => {
-    if (value === 1) map.set(key, 100);
-  });
-};
-
-export default updateUniqueItems;
+export default function updateUniqueItems(map) {
+  if (map instanceof Map) {
+    for (const [key, value] of map) {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    }
+  } else {
+    throw new Error('Cannot process');
+  }
+  return map;
+}
