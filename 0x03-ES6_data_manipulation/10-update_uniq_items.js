eb3c02305dc0f6a195b20abc090996d367ce4952
bu-okem <chiobiegbulem@gmail.com>
2024-08-14 14:58:25 +0100

    fix: task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -3,15 +3,8 @@ export default function updateUniqueItems(map) {
     throw new Error("Cannot process");
   }
 
-  const updatedMap = new Map();
-
-  for (const [key, value] of map.entries()) {
-    if (value && typeof value === "object" && "quantity" in value && value.quantity === 1) {
-      updatedMap.set(key, { ...value, quantity: 100 });
-    } else {
-      updatedMap.set(key, value);
-    }
-  }
-
-  return updatedMap;
+  map.forEach((value, key) => {
+    if (value === 1) map.set(key, 100);
+  });
 }
+
